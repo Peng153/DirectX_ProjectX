61bc3cd7c9bbadcb894952a1889e17739699a397
Peng153 <mysky5653@gmail.com>
2020-08-11 15:49:12 +0900

    commit-message

---
+++ Project_X/Project_X/InputClass.cpp
@@ -0,0 +1,38 @@
+﻿#include "stdafx.h"
+#include "InputClass.h"
+
+InputClass::InputClass()
+{
+}
+
+InputClass::InputClass(const InputClass&)
+{
+}
+
+InputClass::~InputClass()
+{
+}
+
+void InputClass::Initialize()
+{
+	// 키 배열 초기화
+	for (int i = 0; i < 256; ++i) m_key[i] = false;
+}
+
+void InputClass::KeyDown(unsigned int input)
+{
+	// 키가 눌렸을 시 키값 true
+	m_key[input] = true;
+}
+
+void InputClass::KeyUp(unsigned int input)
+{
+	// 키가 해제되었을 시 키값 false
+	m_key[input] = false;
+}
+
+bool InputClass::IsKeyDown(unsigned int key)
+{
+	// 키가 눌렸는지 아닌지 상태 반환
+	return m_key[key];
+}
---
+++ Project_X/Project_X/stdafx.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+
+// Windows 헤더 파일
+#include <Windows.h>
+
+// C 런타임 헤더 파일
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+#include "DxDefine.h"
